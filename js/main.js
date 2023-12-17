2a6ea1df1001991786950d3bb9cc7da7471954eb
Iaroslav <iaroslavpiatak@gmail.com>
2023-12-17 11:25:56 +0300

    add title-block

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 let menu_icon = document.querySelector('.header__menu-icon');
 let mobile_menu = document.querySelector('.header__menu');
-let wrapper = document.querySelector('.wrapper')
+let body = document.querySelector('.body');
+
 
 menu_icon.addEventListener('click', ()=> {
     menu_icon.classList.toggle('header__menu-icon_open');
     mobile_menu.classList.toggle('header__menu_open');
-    wrapper.classList.toggle('hidden-content')
+    body.classList.toggle('hidden-content')
 })
