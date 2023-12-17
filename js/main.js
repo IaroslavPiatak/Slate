51c8499d59d4585310e98b16ab9b4906f332e961
Iaroslav <iaroslavpiatak@gmail.com>
2023-12-17 16:52:39 +0300

    add possibilities

--- js/main.js
+++ js/main.js
@@ -8,3 +8,15 @@ menu_icon.addEventListener('click', ()=> {
     mobile_menu.classList.toggle('header__menu_open');
     body.classList.toggle('hidden-content')
 })
+
+const mediaQuery = window.matchMedia('(max-width: 768px)')
+function handleTabletChange(e) {
+  if (e.matches) {
+    document.querySelector('.possibilities__img').innerHTML = '<img src="img/main/possibilities/big-picture_mobile.svg" alt="abracadabra">'
+  }
+  else {
+    document.querySelector('.possibilities__img').innerHTML = '<img src="img/main/possibilities/big-picture.svg" alt="abracadabra">'
+  }
+}
+mediaQuery.addListener(handleTabletChange)
+handleTabletChange(mediaQuery)
