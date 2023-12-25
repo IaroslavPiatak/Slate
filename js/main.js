cd21b1d0fc56502caa342ae9c3e2b6f614b3e6b3
Iaroslav <iaroslavpiatak@gmail.com>
2023-12-25 17:37:09 +0300

    add gallery

--- js/main.js
+++ js/main.js
@@ -13,9 +13,18 @@ const mediaQuery = window.matchMedia('(max-width: 768px)')
 function handleTabletChange(e) {
   if (e.matches) {
     document.querySelector('.possibilities__img').innerHTML = '<img src="img/main/possibilities/big-picture_mobile.svg" alt="abracadabra">'
+    document.querySelector('.gallery-content__img-1').innerHTML = '<img src="img/main/gallery/gallery_img_mobile_1.png" alt="abracadabra">'
+    document.querySelector('.gallery-content__img-2').innerHTML = '<img src="img/main/gallery/gallery_img_mobile_2.png" alt="abracadabra">'
+    document.querySelector('.gallery-content__img-3').innerHTML = '<img src="img/main/gallery/gallery_img_mobile_3.png" alt="abracadabra">'
+    document.querySelector('.gallery-content__img-4').innerHTML = '<img src="img/main/gallery/gallery_img_mobile_4.png" alt="abracadabra">'
+    
   }
   else {
     document.querySelector('.possibilities__img').innerHTML = '<img src="img/main/possibilities/big-picture.svg" alt="abracadabra">'
+    document.querySelector('.gallery-content__img-1').innerHTML = '<img src="img/main/gallery/gallery_img-1.png" alt="abracadabra">'
+    document.querySelector('.gallery-content__img-2').innerHTML = '<img src="img/main/gallery/gallery_img-2.png" alt="abracadabra">'
+    document.querySelector('.gallery-content__img-3').innerHTML = '<img src="img/main/gallery/gallery_img-3.png" alt="abracadabra">'
+    document.querySelector('.gallery-content__img-4').innerHTML = '<img src="img/main/gallery/gallery_img-4.png" alt="abracadabra">'
   }
 }
 mediaQuery.addListener(handleTabletChange)
